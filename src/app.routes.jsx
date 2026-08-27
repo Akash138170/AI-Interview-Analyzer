dd1b9ac7bb8a2071853b543e5281808c48a997e8
Akash Rajput <akashkumar8298098@gmail.com>
2026-08-27 23:33:57 +0530

    fix login import path

--- src/app.routes.jsx
+++ src/app.routes.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router";
 
-import Login from "./features/auth/pages/login";
+import Login from "./features/auth/pages/Login"
 import Register from "./features/auth/pages/Register";
 import Protected from "./features/auth/component/Protected";
 import Home from "./features/interview/pages/Home";
